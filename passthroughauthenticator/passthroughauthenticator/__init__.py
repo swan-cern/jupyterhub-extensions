111c7e5ddcb60270228d132b8a020a99ae936b58
Danilo Piparo <danilo.piparo@cern.ch>
2015-11-23 11:04:38 +0100

    Refactor code

--- passthroughauthenticator/passthroughauthenticator/__init__.py
+++ passthroughauthenticator/passthroughauthenticator/__init__.py
@@ -1,18 +1,1 @@
-"""Pass through authenticator"""
-
-from jupyterhub.auth import LocalAuthenticator
-from traitlets import Unicode
-from tornado import gen, web
-
-
-class PassThroughAuthenticator(LocalAuthenticator):
-    """Authenticate a user without checkoing the password"""
-    service = Unicode('login', config=True,
-        help="""The authentication without password checking."""
-    )
-
-    @gen.coroutine
-    def authenticate(self, handler, data):
-        """Return the username and skip authentication."""
-        print (data['username'])
-        return data['username']
+from .passthroughauth import PassThroughAuthenticator
---
+++ passthroughauthenticator/passthroughauthenticator/passthroughauthenticator.py
@@ -0,0 +1,19 @@
+"""Pass through authenticator"""
+
+from jupyterhub.auth import LocalAuthenticator
+from traitlets import Unicode
+from tornado import gen, web
+
+
+class PassThroughAuthenticator(LocalAuthenticator):
+    """Authenticate a user without checkoing the password"""
+    service = Unicode('login', config=True,
+        help="""The authentication without password checking."""
+    )
+
+    @gen.coroutine
+    def authenticate(self, handler, data):
+        """Return the username and skip authentication."""
+        username = data['username']
+        self.log.debug("Letting user %s pass through.", username)
+        return username
