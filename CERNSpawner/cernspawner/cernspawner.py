69c71bfcd9fdc195080b0089275eff124ffbed30
Danilo Piparo <danilo.piparo@cern.ch>
2015-12-11 17:12:00 +0100

    Allow to select users allowed to login with a list pointed by the AUTHUSERS env var

--- CERNSpawner/cernspawner/cernspawner.py
+++ CERNSpawner/cernspawner/cernspawner.py
@@ -13,11 +13,11 @@ class CERNSpawner(SystemUserSpawner):
         """Start the container and perform the operations necessary for mounting
         EOS.
         """
-
+        username = self.user.name
         # Create a temporary home for the user.
-        home_dir = "/home/%s" %self.user.name
+        home_dir = "/home/%s" %username
         subprocess.call(["mkdir","-p", home_dir])
-        subprocess.call(["chown", self.user.name, home_dir])
+        subprocess.call(["chown", username, home_dir])
 
         tornadoFuture = super(CERNSpawner, self).start(
             image=image
@@ -25,14 +25,15 @@ class CERNSpawner(SystemUserSpawner):
 
         def get_and_bind_ticket(dummy):
             # Temporary limitation
-            if not self.user.name in ["dpiparo","etejedor","dmaas","rw15u095"]: return
+            allowed_users = map(lambda s: s[:-1], open(os.environ["AUTHUSERS"]).readlines())
+            if not username in allowed_users: return
             resp = self.docker('inspect_container',self.container_id)
             if not resp:
                 self.log.warn("Container not found")
             container = resp.result()
             container_pid = container['State']['Pid']
-            self.log.debug("We are in CERNSpawner. Container requested by %s has pid %s.", self.user.name, container_pid)
-            subprocess.call([os.environ["AUTHSCRIPT"], self.user.name, str(container_pid)])
+            self.log.debug("We are in CERNSpawner. Container requested by %s has pid %s.", username, container_pid)
+            subprocess.call([os.environ["AUTHSCRIPT"], username, str(container_pid)])
 
         tornadoFuture.add_done_callback(get_and_bind_ticket)
         yield tornadoFuture
