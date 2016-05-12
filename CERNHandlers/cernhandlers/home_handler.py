d5de262ac9d394d6ca3aac85f77b5908ceecd283
Danilo Piparo <danilo.piparo@cern.ch>
2016-05-12 17:04:30 +0200

    Allow to SWAN single notebooks if user is running

--- CERNHandlers/cernhandlers/home_handler.py
+++ CERNHandlers/cernhandlers/home_handler.py
@@ -3,6 +3,9 @@
 
 """CERN Home handler"""
 
+import os
+import subprocess
+
 from tornado import web, gen
 
 from jupyterhub.handlers.base import BaseHandler
@@ -14,11 +17,25 @@ class HomeHandler(BaseHandler):
 
     @web.authenticated
     def get(self):
+        the_user = self.get_current_user()
         the_projurl = self.get_argument('projurl','')
+        the_home_url = ''
         if the_projurl:
            check_url(the_projurl)
+           if the_user.running:
+               the_user_name = the_user.name
+               self.log.info('User %s is running. Fetching project %s.' %(the_user_name,the_projurl))
+               command = 'sudo /srv/jupyterhub/fetcher/fetcher.py %s %s %s' %(the_projurl,the_user_name,'SWAN_projects')
+               self.log.info('Calling command: %s' %command)
+               subprocess.call(command.split())
+               proj_name = os.path.basename(the_projurl)
+               if proj_name.endswith('.ipynb'):
+                   the_home_url = os.path.join('SWAN_projects',proj_name)
+
         html = self.render_template('home.html',
-            user=self.get_current_user(),
-            projurl = the_projurl
+            user = the_user,
+            projurl = the_projurl,
+            home_url = the_home_url
         )
+
         self.finish(html)
