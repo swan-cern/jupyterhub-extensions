916357ae31c83dc88bb2f8dd2413c56f1305ea29
Danilo Piparo <danilo.piparo@cern.ch>
2016-05-13 14:56:30 +0200

    Manage download of project in spawn handler
    the functionality is implemented for two cases.
    1. The user is running
    The method hit is "get". The project name is inferred and the
    file properly downloaded. The user is redirected to the correct url.
    2. The user is *not* running
    The get method is hit. The form is enriched with a hidden txt field
    with the name of the project inside. Upon hitting the spwan button,
    the post method is hit. The project name is inferred from the hidden
    field, the mechanism to download the files is triggered and the
    user properly redirected.

--- CERNHandlers/cernhandlers/spawn_handler.py
+++ CERNHandlers/cernhandlers/spawn_handler.py
@@ -3,6 +3,9 @@
 
 """CERN Spawn handler"""
 
+import os
+import subprocess
+
 from tornado import web, gen
 from tornado.httputil import url_concat
 
@@ -18,28 +21,52 @@ class SpawnHandler(BaseHandler):
 
     Only enabled when Spawner.options_form is defined.
     """
+
     def _render_form(self, message=''):
         user = self.get_current_user()
+        # We inject an extra field if there is a project set
+        the_projurl = self.get_argument('projurl','')
+        the_form = user.spawner.options_form
+        if the_projurl:
+            the_form +='<input type="hidden" name="projurl" value="%s">' %the_projurl
         return self.render_template('spawn.html',
             user=user,
-            spawner_options_form=user.spawner.options_form,
+            spawner_options_form=the_form,
             error_message=message,
         )
 
-    def url_add_args(self,url):
-       the_projurl = self.get_argument('projurl','')
-       if the_projurl:
-           check_url(the_projurl)
-           url = url_concat(url, {'projurl': projurl})
-       return url
+    def handle_redirection(self, the_projurl = ''):
+        ''' Return redirection url'''
+        if not the_projurl:
+            the_projurl = self.get_argument('projurl','')
+        if not the_projurl: return ''
+
+        check_url(the_projurl)
+
+        the_user = self.get_current_user()
+        if not the_user.running: return ''
+
+        the_user_name = the_user.name
+        self.log.info('User %s is running. Fetching project %s.' %(the_user_name,the_projurl))
+        command = 'sudo /srv/jupyterhub/fetcher/fetcher.py %s %s %s' %(the_projurl,the_user_name,'SWAN_projects')
+        self.log.info('Calling command: %s' %command)
+        subprocess.call(command.split())
+        proj_name = os.path.basename(the_projurl)
+        the_home_url = ''
+        if proj_name.endswith('.ipynb'): # git repo to be added
+            the_home_url = os.path.join('SWAN_projects',proj_name)
+        return the_home_url
 
     @web.authenticated
     def get(self):
         """GET renders form for spawning with user-specified options"""
         user = self.get_current_user()
         if user.running:
-            url = self.url_add_args(user.url)
-            self.log.debug("User is running: %s", url)
+            url = user.url
+            self.log.warning("User is running: %s", url)
+            redirect_url = self.handle_redirection()
+            if redirect_url:
+                url = os.path.join(url, 'tree', redirect_url)
             self.redirect(url)
             return
         if user.spawner.options_form:
@@ -55,8 +82,8 @@ class SpawnHandler(BaseHandler):
         """POST spawns with user-specified options"""
         user = self.get_current_user()
         if user.running:
-            url = self.url_add_args(user.url)
-            self.log.warning("User is already running: %s", url)
+            url = user.url
+            self.log.debug("User is already running: %s", url)
             self.redirect(url)
             return
         form_options = {}
@@ -72,5 +99,10 @@ class SpawnHandler(BaseHandler):
             self.finish(self._render_form(str(e)))
             return
         self.set_login_cookie(user)
-        url = self.url_add_args(user.url)
+        url = user.url
+        projurl_key = 'projurl'
+        if projurl_key in self.request.body_arguments:
+            the_projurl = self.request.body_arguments['projurl'][0]
+            redirect_url = self.handle_redirection(the_projurl)
+            url = os.path.join(url, 'tree', redirect_url)
         self.redirect(url)
