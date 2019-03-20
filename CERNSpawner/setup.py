f2870e7e928c68dc226ad0062d43926f3827e13b
Diogo Castro <diogo.castro@cern.ch>
2019-03-20 21:06:15 +0100

    Make spawner pip installable

--- CERNSpawner/setup.py
+++ CERNSpawner/setup.py
@@ -1,21 +1,8 @@
 #!/usr/bin/env python
-# coding: utf-8
 
-
-#-----------------------------------------------------------------------------
-# Minimal Python version sanity check (from IPython/Jupyterhub)
-#-----------------------------------------------------------------------------
-from __future__ import print_function
-
+from setuptools import setup
 import os
 import sys
-
-v = sys.version_info
-if v[:2] < (3,3):
-    error = "ERROR: Jupyter Hub requires Python version 3.3 or above."
-    print(error, file=sys.stderr)
-    sys.exit(1)
-
 
 if os.name in ('nt', 'dos'):
     error = "ERROR: Windows is not supported"
@@ -34,41 +21,29 @@ with open(pjoin(here, 'version.py')) as f:
     exec(f.read(), {}, version_ns)
 
 
-setup_args = dict(
-    name                = 'cernspawner',
-    packages            = ['cernspawner'],
-    version             = version_ns['__version__'],
-    description         = """CERNSpawner: Get credential and spawn.""",
-    long_description    = "",
-    author              = "Danilo Piparo",
-    author_email        = "danilo.piparo@cern.ch",
-    url                 = "root.cern.ch",
-    license             = "BSD",
-    platforms           = "Linux, Mac OS X",
-    keywords            = ['Interactive', 'Interpreter', 'Shell', 'Web'],
-    classifiers         = [
-        'Intended Audience :: Developers',
-        'Intended Audience :: System Administrators',
-        'Intended Audience :: Science/Research',
-        'License :: OSI Approved :: BSD License',
-        'Programming Language :: Python',
-        'Programming Language :: Python :: 3',
-    ],
-)
-
-# setuptools requirements
-if 'setuptools' in sys.modules:
-    setup_args['install_requires'] = install_requires = []
-    with open('requirements.txt') as f:
-        for line in f.readlines():
-            req = line.strip()
-            if not req or req.startswith(('-e', '#')):
-                continue
-            install_requires.append(req)
-
-
-def main():
-    setup(**setup_args)
-
-if __name__ == '__main__':
-    main()
+setup(name='cernspawner',
+      packages=['cernspawner'],
+      version=version_ns['__version__'],
+      description='Spawner for SWAN',
+      include_package_data=True,
+      zip_safe=False,
+      author="Danilo Piparo",
+      author_email="danilo.piparo@cern.ch",
+      url="root.cern.ch",
+      license="BSD",
+      platforms="Linux, Mac OS X",
+      keywords=['Interactive', 'Interpreter', 'Shell', 'Web'],
+      classifiers=[
+          'Intended Audience :: Developers',
+          'Intended Audience :: System Administrators',
+          'Intended Audience :: Science/Research',
+          'License :: OSI Approved :: BSD License',
+          'Programming Language :: Python',
+          'Programming Language :: Python :: 3',
+      ],
+      install_requires=[
+          'setuptools',
+          'jupyterhub',
+          'dockerspawner==0.10.*',
+      ],
+      )
