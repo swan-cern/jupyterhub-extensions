96ebe66a50c6e462e13a00313f64521809e0a73f
Prasanth Kothuri <prasanth.kothuri@cern.ch>
2020-04-16 08:41:59 +0200

    Move to KeyCloak

--- scripts/start_proxy.sh
+++ scripts/start_proxy.sh
@@ -2,4 +2,4 @@
 
 # Wrap the call of configurable-http-proxy to set the max header size.
 # Remove the first option (--ip) to allow the proxy to listen on all the interfaces.
-node --max-http-header-size=16000 /usr/bin/configurable-http-proxy ${@:2}
+node --max-http-header-size=16000 /usr/bin/configurable-http-proxy --redirect-port 80 --redirect-to 443 ${@:2}
