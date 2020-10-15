56b8034c05b7343ebdf0124c5cbd9084f18cacfc
Diogo Castro <diogo.castro@cern.ch>
2020-10-15 21:08:59 +0200

    SwanSpawner: installable

--- SwanSpawner/setup.py
+++ SwanSpawner/setup.py
@@ -1,19 +1,48 @@
-#!/usr/bin/env python
+import os
 
-from setuptools import setup, find_packages
+from jupyter_packaging import get_version
+import setuptools
 
-setup(name='swanspawner',
-      version='1.2',
-      description='Spawner for SWAN',
-      include_package_data=True,
-      packages=find_packages(),
-      package_data={'swanspawner': ['templates/*']},
-      zip_safe=False,
-      install_requires=[
+name="swanspawner"
+
+# Get our version
+version = get_version(os.path.join(name, "_version.py"))
+
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup_args = dict(
+    name=name,
+    version=version,
+    url="https://github.com/swan-cern/jupyterhub-extensions",
+    author="SWAN Admins",
+    description="SWAN JupyterHub spawner",
+    long_description= long_description,
+    long_description_content_type="text/markdown",
+    packages=setuptools.find_packages(),
+    install_requires=[
         'setuptools',
         'jupyterhub',
         'psutil',
         'dockerspawner==0.11.1',
         'jupyterhub-kubespawner==0.12.0'
-      ],
-  )
+    ],
+    zip_safe=False,
+    include_package_data=True,
+    license="AGPL-3.0",
+    platforms="Linux, Mac OS X",
+    keywords=["JupyterHub", "Spawner", "SWAN", "CERN"],
+    classifiers=[
+        "License :: OSI Approved :: GNU Affero General Public License v3",
+        'Intended Audience :: Developers',
+        'Intended Audience :: System Administrators',
+        'Intended Audience :: Science/Research',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+    ],
+)
+
+
+if __name__ == "__main__":
+    setuptools.setup(**setup_args)
