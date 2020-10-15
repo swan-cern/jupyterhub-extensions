381d8d7afe0588443d49902169172da421659154
Diogo Castro <diogo.castro@cern.ch>
2020-10-15 20:01:35 +0200

    SwanNotificationsService: installable

--- SwanNotificationsService/setup.py
+++ SwanNotificationsService/setup.py
@@ -1,15 +1,49 @@
-#!/usr/bin/env python
+import os
 
-from setuptools import setup, find_packages
+from jupyter_packaging import get_version
+import setuptools
 
-setup(name='swannotificationsservice',
-      version='0.0.1',
-      description='Server feeding notifications for SWAN',
-      include_package_data=True,
-      packages=find_packages(),
-      zip_safe=False,
-      install_requires=[
-          'jupyterhub',
-          'tornado'
-      ],
-  )
+name="swannotificationsservice"
+
+# Get our version
+version = get_version(os.path.join(name, "_version.py"))
+
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup_args = dict(
+    name=name,
+    version=version,
+    url="https://github.com/swan-cern/jupyterhub-extensions",
+    author="SWAN Admins",
+    description="JupyterHub service to expose notifications for Jupyter",
+    long_description= long_description,
+    long_description_content_type="text/markdown",
+    packages=setuptools.find_packages(),
+    install_requires=[
+          'jupyterhub'
+    ],
+    zip_safe=False,
+    include_package_data=True,
+    license="AGPL-3.0",
+    platforms="Linux, Mac OS X",
+    keywords=["JupyterHub", "SWAN", "CERN"],
+    entry_points={
+        'console_scripts': [
+            'swannotificationsservice = swannotificationsservice.app:main',
+        ],
+    },
+    classifiers=[
+        "License :: OSI Approved :: GNU Affero General Public License v3",
+        'Intended Audience :: Developers',
+        'Intended Audience :: System Administrators',
+        'Intended Audience :: Science/Research',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+    ],
+)
+
+
+if __name__ == "__main__":
+    setuptools.setup(**setup_args)
