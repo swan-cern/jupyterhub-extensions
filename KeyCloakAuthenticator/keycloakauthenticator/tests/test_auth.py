6f5f1430636ba9b3b515584c169eef85b9f39fa7
Krishnan R <krishnanr1997@gmail.com>
2022-09-19 16:38:37 +0200

    Add test case for expired refresh token

--- KeyCloakAuthenticator/keycloakauthenticator/tests/test_auth.py
+++ KeyCloakAuthenticator/keycloakauthenticator/tests/test_auth.py
@@ -11,14 +11,15 @@ def _generate_mock_public_private_key_pair():
     return (public_key, private_key)
 
 
-def _get_mock_token(private_key, token_id):
+def _get_mock_token(private_key, token_id, expired=False):
+    expiry = 300 if expired else 9999999999
     return jwt.encode(
         payload={
             "sub": "dummy-subject",
             "iss": "dummy-oidc-url",
             "aud": "dummy-client-id",
             "iat": 0,  # Issued a long time ago: 1/1/1970
-            "exp": 9999999999,  # One long-lasting token, expiring 11/20/2286
+            "exp": expiry,
             "permissions": ["read", "write"],
             "jti": token_id,
         },
@@ -55,7 +56,9 @@ async def test_refresh_user(monkeypatch):
 
         async def get_auth_state(self):
             return {
-                "access_token": _get_mock_token(private_key, "old_access_token"),
+                "access_token": _get_mock_token(
+                    private_key, "old_access_token", expired=True
+                ),
                 "refresh_token": _get_mock_token(private_key, "old_refresh_token"),
             }
 
@@ -76,3 +79,50 @@ async def test_refresh_user(monkeypatch):
     assert updated_auth_state["auth_state"]["access_token"] == _get_mock_token(
         private_key, "new_access_token"
     )
+
+
+@pytest.mark.asyncio
+async def test_refresh_user_with_expired_refresh_token(monkeypatch):
+
+    public_key, private_key = _generate_mock_public_private_key_pair()
+
+    async def mock_get_oidc_config(self):
+        pass
+
+    monkeypatch.setattr(
+        KeyCloakAuthenticator, "_get_oidc_configs", mock_get_oidc_config
+    )
+    monkeypatch.setattr(KeyCloakAuthenticator, "oidc_issuer", "dummy-oidc-url")
+
+    async def fetch(self, request, label):
+        print("Mocking fetch for ", request, label)
+        return {
+            "access_token": _get_mock_token(private_key, "new_access_token"),
+            "refresh_token": _get_mock_token(private_key, "new_refresh_token"),
+        }
+
+    monkeypatch.setattr(KeyCloakAuthenticator, "fetch", fetch)
+
+    class MockUser:
+        name = "dummy-user"
+
+        async def get_auth_state(self):
+            return {
+                "access_token": _get_mock_token(
+                    private_key, "old_access_token", expired=True
+                ),
+                "refresh_token": _get_mock_token(
+                    private_key, "old_refresh_token", expired=True
+                ),
+            }
+
+    authenticator = KeyCloakAuthenticator()
+    authenticator.configured = True
+    authenticator.public_key = public_key
+    authenticator.client_id = "dummy-client-id"
+    authenticator.client_secret = "dummy-client-secret"
+    authenticator.exchange_tokens = ["another-service-audience"]
+
+    refresh_result = await authenticator.refresh_user(MockUser())
+
+    assert refresh_result is False
