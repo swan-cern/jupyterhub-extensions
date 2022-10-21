8a81edd572a945284b5ed579876441e6cffece56
Krishnan R <krishnanr1997@gmail.com>
2022-10-21 12:16:05 +0200

    Add docstrings

--- KeyCloakAuthenticator/keycloakauthenticator/tests/test_auth.py
+++ KeyCloakAuthenticator/keycloakauthenticator/tests/test_auth.py
@@ -6,12 +6,14 @@ from ..auth import KeyCloakAuthenticator
 
 
 def _generate_mock_public_private_key_pair():
+    """Create a mock public/private key pair for faking JWTs from the server"""
     private_key = rsa.generate_private_key(public_exponent=65537, key_size=2048)
     public_key = private_key.public_key()
     return (public_key, private_key)
 
 
 def _get_mock_token(private_key, token_id, expired=False):
+    """Create a fake JWT issued by the server, that is valid if expired=False"""
     expiry = 300 if expired else 9999999999
     return jwt.encode(
         payload={
@@ -31,12 +33,18 @@ def _get_mock_token(private_key, token_id, expired=False):
 
 @pytest.mark.asyncio
 async def test_refresh_user(monkeypatch):
+    """
+    Test KeyCloakAuthenticator.refresh_user() when everything works fine.
+    Mocks the initial configuration with the server, and HTTP requests with a fake JWTs
+    """
 
     public_key, private_key = _generate_mock_public_private_key_pair()
 
     async def mock_get_oidc_config(self):
         pass
 
+    # The authenticator fetches OIDC configuration from KeyCloak on startup
+    # mock that step, and configure some dummy configuration
     monkeypatch.setattr(
         KeyCloakAuthenticator, "_get_oidc_configs", mock_get_oidc_config
     )
@@ -49,9 +57,11 @@ async def test_refresh_user(monkeypatch):
             "refresh_token": _get_mock_token(private_key, "new_refresh_token"),
         }
 
+    # Mock the response from the server on refresh and exchange tokens
     monkeypatch.setattr(KeyCloakAuthenticator, "fetch", fetch)
 
     class MockUser:
+        """Fake user object, with dummy authentication state from DB"""
         name = "dummy-user"
 
         async def get_auth_state(self):
@@ -83,15 +93,24 @@ async def test_refresh_user(monkeypatch):
 
 @pytest.mark.asyncio
 async def test_refresh_user_with_expired_refresh_token(monkeypatch):
+    """
+    Test KeyCloakAuthenticator.refresh_user() when the refresh_token stored in the users auth_state
+    in the DB is expired, expecting the refresh to fail and return false.
+
+    Mocks the initial configuration with the server, and HTTP requests with a fake JWTs that are expired
+    """
 
     public_key, private_key = _generate_mock_public_private_key_pair()
 
     async def mock_get_oidc_config(self):
         pass
 
+    # The authenticator fetches OIDC configuration from KeyCloak on startup
+    # mock that step, and configure some dummy configuration
     monkeypatch.setattr(
         KeyCloakAuthenticator, "_get_oidc_configs", mock_get_oidc_config
     )
+    # Mock the response from the server on refresh and exchange tokens
     monkeypatch.setattr(KeyCloakAuthenticator, "oidc_issuer", "dummy-oidc-url")
 
     async def fetch(self, request, label):
@@ -104,6 +123,7 @@ async def test_refresh_user_with_expired_refresh_token(monkeypatch):
     monkeypatch.setattr(KeyCloakAuthenticator, "fetch", fetch)
 
     class MockUser:
+        """Fake user object, with dummy authentication state from DB"""
         name = "dummy-user"
 
         async def get_auth_state(self):
