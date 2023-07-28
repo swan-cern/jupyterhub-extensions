fc0fb8ea4e0e434a1a844d4e4feb25010ce6567e
Enric Tejedor Saavedra <enric.tejedor.saavedra@cern.ch>
2023-07-28 11:09:28 +0200

    Move post_stop_hook cleanup code to swankubespawner's stop
    This overrides the stop method of kubespawner to include some
    cleanup of secrets and services related to EOS and Spark after
    the pod has terminated.
    
    This was previously done in two post_stop_hook functions defined
    in swan_config_cern.py and swan_spark_config.py in the swan-cern
    chart. After moving to an async spawner, that hook can't be used
    anymore because it can't be a coroutine (it is never awaited).
    However, the moved code needs to be in a coroutine because it
    includes calls to the k8s API that need to be awaited. This
    justifies its transfer to stop, which is a coroutine.

--- SwanSpawner/swanspawner/swankubespawner.py
+++ SwanSpawner/swanspawner/swankubespawner.py
@@ -1,6 +1,9 @@
 from .swanspawner import define_SwanSpawner_from
 from kubespawner import KubeSpawner
 
+from kubernetes_asyncio.client.rest import ApiException
+
+import os
 from math import ceil
 from traitlets import Float
 
@@ -47,6 +50,40 @@ class SwanKubeSpawner(define_SwanSpawner_from(KubeSpawner)):
         except BaseException as e:
             self.log.error("Error while spawning the user container: %s", e, exc_info=True)
             raise e
+
+    async def stop(self, now=False):
+        '''Do custom cleanup after terminating user pod'''
+        try:
+            await super().stop()
+        finally:
+            # Delete Kubernetes secret storing EOS kerberos ticket of the user
+            username = self.user.name
+            eos_secret_name = f'eos-tokens-{username}'
+            namespace = os.environ.get('POD_NAMESPACE', 'default')
+            self.log.info(f'Deleting secret {namespace}:{eos_secret_name}')
+            try:
+                await self.api.delete_namespaced_secret(eos_secret_name, namespace)
+            except ApiException as e:
+                self.log.error('Error deleting secret {namespace}:{eos_secret_name}: {e}')
+
+            # Spark-related cleanup
+            spark_cluster = self.user_options[self.spark_cluster_field]
+            if spark_cluster and spark_cluster != 'none':
+                # Delete NodePort service opening ports for the user Spark processes
+                spark_ports_service = f'spark-ports-{username}'
+                self.log.info(f'Deleting service {namespace}:{spark_ports_service}')
+                try:
+                    await self.api.delete_namespaced_service(spark_ports_service, namespace)
+                except ApiException as e:
+                    self.log.error('Error deleting service {namespace}:{spark_ports_service}: {e}')
+
+                # Delete Kubernetes secret with Hadoop delegation tokens
+                hadoop_secret_name = f'hadoop-tokens-{username}'
+                self.log.info(f'Deleting secret {namespace}:{hadoop_secret_name}')
+                try:
+                    await self.api.delete_namespaced_secret(hadoop_secret_name, namespace)
+                except ApiException as e:
+                    self.log.error('Error deleting secret {namespace}:{hadoop_secret_name}: {e}')
 
     def get_env(self):
         """ Set base environmental variables for swan jupyter docker image """
