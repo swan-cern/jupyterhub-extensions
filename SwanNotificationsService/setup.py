42852e6e738adead861151071e9e4d5f8cbd0398
Enric Tejedor Saavedra <enric.tejedor.saavedra@cern.ch>
2024-08-28 14:13:15 +0200

    [SwanNotificationsService] Update jupyterhub version to 4.1.6

--- SwanNotificationsService/setup.py
+++ SwanNotificationsService/setup.py
@@ -22,7 +22,7 @@ setup_args = dict(
     long_description_content_type="text/markdown",
     packages=setuptools.find_packages(),
     install_requires=[
-          'jupyterhub~=4.0.0',
+          'jupyterhub~=4.1.6',
           'kubernetes~=22.0'
     ],
     zip_safe=False,
