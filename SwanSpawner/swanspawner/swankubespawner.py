2d33cef76b3366f74b11d0ac75734f6993963758
Laura Llinares <llinares.laura@cern.ch>
2025-06-18 15:06:25 +0200

    [SwanSpawner]: show free GPU model to users
    The availability count is decreased in the creation hook, and the increased when the stop() method is invoked. The availability count is recalculated every 10 mins to ensure consistency.

--- SwanSpawner/swanspawner/swankubespawner.py
+++ SwanSpawner/swanspawner/swankubespawner.py
@@ -108,12 +108,23 @@ class SwanKubeSpawner(define_SwanSpawner_from(KubeSpawner)):
                     except ApiException as e:
                         self.log.error('Error deleting secret {namespace}:{hadoop_secret_name}: {e}')
 
+            # free GPU update
+            gpu_flavour = self.user_options.get('gpu')
+            if gpu_flavour and gpu_flavour.lower() != 'none':
+                try:
+                    self.gpus._update_free_gpu_flavours()
+                    self.log.info(f"Update free GPU count for {gpu_flavour} after user stop.")
+                except Exception as e:
+                    self.log.error(f"Failed to update free GPU count: {e}")
+
     def _render_templated_options_form(self, spawner):
         """
         Adds dynamic GPU information to render as part of the options form
         """
-        gpu_flavours = self.gpus.get_gpu_flavours()
+        gpu_flavours = self.gpus.get_available_gpu_flavours()
+        free_gpu_flavours = self.gpus.get_free_gpu_flavours()
         # Sort flavours by count so the most common one appears first in the list,
         # and therefore is rendered first in the form.
-        self._dynamic_form_info['gpu_flavours'] = sorted(gpu_flavours, key=lambda x: gpu_flavours.get(x).count, reverse=True)
+        self._dynamic_form_info['gpu_flavours'] = list(gpu_flavours.keys())
+        self._dynamic_form_info['free_gpu_flavours'] = sorted(free_gpu_flavours, key=lambda x: free_gpu_flavours.get(x, 0),reverse=True)
         return super()._render_templated_options_form(spawner)
