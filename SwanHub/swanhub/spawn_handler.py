3d557c8ab6cb5f9ffa16769a89d3470c5cebb396
RodrigoSobral2000 <rodrigosobral@sapo.pt>
2025-06-20 10:48:29 +0200

    swanhub: Simplification of redirection to the user sessions

--- SwanHub/swanhub/spawn_handler.py
+++ SwanHub/swanhub/spawn_handler.py
@@ -49,14 +49,7 @@ class SpawnHandler(JHSpawnHandler):
             # User has a session running, redirect to the correct page,
             # according to the user's choice and the current session
             spawner_software_source = spawner.user_options.get(configs.software_source)
-            if spawner_software_source == configs.customenv_special_type:
-                next_url = url_path_join("user", user.escaped_name, "customenvs", server_name)
-            else:
-                use_jupyterlab = spawner.user_options.get(configs.use_jupyterlab_field)
-                if use_jupyterlab == 'checked':
-                    next_url = url_path_join("user", user.escaped_name, "lab")
-                else:
-                    next_url = url_path_join("user", user.escaped_name, "projects")
+            next_url = url_path_join("user", user.escaped_name, "customenvs" if spawner_software_source == configs.customenv_special_type else "")
 
             page = await self.render_template('spawn_conflict.html', for_user=user, spawner=spawner, next_url=next_url)
             self.finish(page)
