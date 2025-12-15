c1e85bbf4a0ab8b4e8224d2eddd1a25d1327b69d
Tomas R. <tomas.roun8@gmail.com>
2025-12-15 10:12:11 +0100

    Create a development setup (#200)
    * Add missing dependency
    
    * Create a development setup

--- SwanSpawner/swanspawner/__init__.py
+++ SwanSpawner/swanspawner/__init__.py
@@ -1,3 +1,10 @@
+import os
+
 from ._version import __version__ 
 from .swandockerspawner import *
-from .swankubespawner import *
+
+
+# KubeSpawner must run in a k8s environment, otherwise it fails to import
+# so only import it when not in dev mode.
+if os.environ.get('SWANHUB_ENV') != 'dev':
+    from .swankubespawner import *
