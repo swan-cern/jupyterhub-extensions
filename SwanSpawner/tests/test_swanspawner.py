f309643af56c37bf16c991c96999d4d5f99952f7
Felix Schatzl <fschatzl@cern.ch>
2026-08-04 14:22:35 +0200

    validate Rucio options tests

--- SwanSpawner/tests/test_swanspawner.py
+++ SwanSpawner/tests/test_swanspawner.py
@@ -5,7 +5,41 @@ Unit tests for swanspawner.py functions
 import os
 
 import pytest
-from swanspawner.swanspawner import get_repo_name_from_options
+from swanspawner.swanspawner import define_SwanSpawner_from, get_repo_name_from_options
+
+_SwanSpawner = define_SwanSpawner_from(object)
+
+
+class _MockLog:
+    def __init__(self):
+        self.warnings = []
+
+    def info(self, *args, **kwargs):
+        pass
+
+    def warning(self, msg, *args, **kwargs):
+        self.warnings.append(msg)
+
+    def error(self, *args, **kwargs):
+        pass
+
+
+class _MockSpawner:
+    """Minimal stand-in; provides the class-level string constants and a log."""
+
+    rucio_instance = 'rucio'
+    rucio_rse = 'rucioRSE'
+    rucio_rse_mount_path = 'rse_mount_path'
+    rucio_path_begins_at = 'path_begins_at'
+
+    def __init__(self):
+        self.log = _MockLog()
+
+
+def _call_validate_rucio(selection, options):
+    mock = _MockSpawner()
+    result = _SwanSpawner._validate_rucio_options(mock, selection, options)
+    return result, mock.log
 
 
 class TestGetRepoNameFromOptions:
@@ -61,3 +95,107 @@ class TestGetRepoNameFromOptions:
         user_options = {'repository': repo_url}
         result = get_repo_name_from_options(user_options)
         assert result == os.path.join(self.PROJECT_FOLDER, expected_name)
+
+
+# ---------------------------------------------------------------------------
+# TestValidateRucioOptions
+# ---------------------------------------------------------------------------
+
+class TestValidateRucioOptions:
+    def test_none_instance_returns_defaults(self):
+        result, _ = _call_validate_rucio({}, {'rucio': 'none'})
+        assert result == {
+            'rucio': 'none',
+            'rucioRSE': 'none',
+            'rse_mount_path': '',
+            'path_begins_at': '0',
+        }
+
+    def test_missing_rucio_config_in_selection_raises(self):
+        with pytest.raises(ValueError, match="Rucio configuration not found"):
+            _call_validate_rucio({}, {'rucio': 'atlas'})
+
+    def test_unknown_rucio_instance_raises(self):
+        selection = {'rucio': [{'value': 'cms', 'rse_options': []}]}
+        with pytest.raises(ValueError, match="Invalid Rucio instance: atlas"):
+            _call_validate_rucio(selection, {'rucio': 'atlas'})
+
+    def test_invalid_rse_raises(self):
+        selection = {'rucio': [{'value': 'atlas', 'rse_options': [
+            {'value': 'ATLAS_CERN', 'rse_mount_path': '/eos/atlas', 'path_begins_at': 3},
+        ]}]}
+        with pytest.raises(ValueError, match="Invalid RSE selection"):
+            _call_validate_rucio(selection, {'rucio': 'atlas', 'rucioRSE': 'UNKNOWN_RSE'})
+
+    def test_valid_selection_returns_correct_options(self):
+        selection = {'rucio': [{'value': 'atlas', 'rse_options': [
+            {'value': 'ATLAS_CERN', 'rse_mount_path': '/eos/atlas', 'path_begins_at': 3},
+        ]}]}
+        result, _ = _call_validate_rucio(
+            selection,
+            {'rucio': 'atlas', 'rucioRSE': 'ATLAS_CERN', 'rse_mount_path': '/eos/atlas', 'path_begins_at': '3'},
+        )
+        assert result == {
+            'rucio': 'atlas',
+            'rucioRSE': 'ATLAS_CERN',
+            'rse_mount_path': '/eos/atlas',
+            'path_begins_at': '3',
+        }
+
+    def test_missing_rse_mount_path_defaults_to_empty_string(self):
+        selection = {'rucio': [{'value': 'atlas', 'rse_options': [
+            {'value': 'ATLAS_CERN'},
+        ]}]}
+        result, _ = _call_validate_rucio(
+            selection, {'rucio': 'atlas', 'rucioRSE': 'ATLAS_CERN', 'rse_mount_path': ''},
+        )
+        assert result['rse_mount_path'] == ''
+
+    def test_missing_path_begins_at_defaults_to_zero_string(self):
+        selection = {'rucio': [{'value': 'atlas', 'rse_options': [
+            {'value': 'ATLAS_CERN', 'rse_mount_path': '/eos/atlas'},
+        ]}]}
+        result, _ = _call_validate_rucio(
+            selection, {'rucio': 'atlas', 'rucioRSE': 'ATLAS_CERN', 'rse_mount_path': '/eos/atlas', 'path_begins_at': '0'},
+        )
+        assert result['path_begins_at'] == '0'
+
+    def test_path_begins_at_int_in_config_is_returned_as_string(self):
+        selection = {'rucio': [{'value': 'atlas', 'rse_options': [
+            {'value': 'ATLAS_CERN', 'rse_mount_path': '/eos/atlas', 'path_begins_at': 5},
+        ]}]}
+        result, _ = _call_validate_rucio(
+            selection, {'rucio': 'atlas', 'rucioRSE': 'ATLAS_CERN', 'rse_mount_path': '/eos/atlas', 'path_begins_at': '5'},
+        )
+        assert result['path_begins_at'] == '5'
+        assert isinstance(result['path_begins_at'], str)
+
+    def test_mount_path_mismatch_logs_warning(self):
+        selection = {'rucio': [{'value': 'atlas', 'rse_options': [
+            {'value': 'ATLAS_CERN', 'rse_mount_path': '/eos/atlas', 'path_begins_at': 3},
+        ]}]}
+        _, log = _call_validate_rucio(
+            selection,
+            {'rucio': 'atlas', 'rucioRSE': 'ATLAS_CERN', 'rse_mount_path': '/wrong/path', 'path_begins_at': '3'},
+        )
+        assert any('Mount path mismatch' in w for w in log.warnings)
+
+    def test_path_begins_at_mismatch_logs_warning(self):
+        selection = {'rucio': [{'value': 'atlas', 'rse_options': [
+            {'value': 'ATLAS_CERN', 'rse_mount_path': '/eos/atlas', 'path_begins_at': 3},
+        ]}]}
+        _, log = _call_validate_rucio(
+            selection,
+            {'rucio': 'atlas', 'rucioRSE': 'ATLAS_CERN', 'rse_mount_path': '/eos/atlas', 'path_begins_at': '99'},
+        )
+        assert any('Path begins at mismatch' in w for w in log.warnings)
+
+    def test_matching_hidden_fields_log_no_warning(self):
+        selection = {'rucio': [{'value': 'atlas', 'rse_options': [
+            {'value': 'ATLAS_CERN', 'rse_mount_path': '/eos/atlas', 'path_begins_at': 3},
+        ]}]}
+        _, log = _call_validate_rucio(
+            selection,
+            {'rucio': 'atlas', 'rucioRSE': 'ATLAS_CERN', 'rse_mount_path': '/eos/atlas', 'path_begins_at': '3'},
+        )
+        assert log.warnings == []
