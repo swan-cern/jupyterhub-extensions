8a9f7ddbd1040701823a18f87ecec4680c77a0a3
thozo <75420993+thozo@users.noreply.github.com>
2026-09-16 13:32:26 +0200

    SwanSpawner: Remove `SWANHUB_ENV` and kubespawner top level import (#312)
    * SwanSpawner: Remove `SWANHUB_ENV` and kubespawner top level import
    
    * SwanSpanwer: Fix reference to SwanKubeSpawner

--- SwanSpawner/swanspawner/__init__.py
+++ SwanSpawner/swanspawner/__init__.py
@@ -1,8 +1,1 @@
-import os
-
 from ._version import __version__  # noqa: F401
-
-# KubeSpawner must run in a k8s environment, otherwise it fails to import
-# so only import it when not in dev mode.
-if os.environ.get('SWANHUB_ENV') != 'dev':
-    from .swankubespawner import *
