4a4ba7c8670822d0a6df9798b6eb7a62c78791b5
thozo <75420993+thozo@users.noreply.github.com>
2026-09-18 09:45:43 +0200

    SwanSpawner: remove SWANHUB_ENV from test config

--- SwanSpawner/tests/conftest.py
+++
@@ -1,5 +0,0 @@
-import os
-
-# swanspawner/__init__.py skips the kubernetes spawner import in dev mode,
-# preventing an in-cluster config load during test collection.
-os.environ.setdefault("SWANHUB_ENV", "dev")
