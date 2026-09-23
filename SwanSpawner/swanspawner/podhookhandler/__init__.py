a553a6f505aa24d34759c5d4fedfa918c7ecbc4e
thozo <75420993+thozo@users.noreply.github.com>
2026-09-23 11:40:26 +0200

    SwanSpawner: Move GPU setup pod hooks from swan-charts (#319)
    * SwanSpawner: move base pod hook handler from swan-charts
    
    * SwanSpawner: use _all__ instead of import alias
    
    * SwanSpawner: move TN pod hook checks from swan-charts
    
    * SwanSpawner: move EOS pod hooks from swan-charts
    
    * SwanSpawner: move GPU pod hooks from swan-charts

--- SwanSpawner/swanspawner/podhookhandler/__init__.py
+++ SwanSpawner/swanspawner/podhookhandler/__init__.py
@@ -1,5 +1,6 @@
 from .swaneospodhookhandler import SwanEosPodHookHandler
+from .swangpupodhookhandler import SwanGPUPodHookHandler
 from .swanlabelpodhookhandler import SwanLabelPodHookHandler
 from .swantnpodhookhandler import SwanTNPodHookHandler
 
-__all__ = ["SwanEosPodHookHandler", "SwanLabelPodHookHandler", "SwanTNPodHookHandler"]
+__all__ = ["SwanEosPodHookHandler", "SwanGPUPodHookHandler", "SwanLabelPodHookHandler", "SwanTNPodHookHandler"]
